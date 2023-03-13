5ea276e89bf0fb91e8dbdef0c843abe57ed3859b
armiy <armachenanbessa26@gmail.com>
2023-03-13 16:40:18 -0400

    iterate 2D array

--- src/javaWorks/HWork.java
+++ src/javaWorks/HWork.java
@@ -1,6 +1,7 @@
 package javaWorks;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class HWork {
@@ -17,8 +18,8 @@ public class HWork {
     public static void printNumbersTriangle() {
         //To print the numbers in a triangle.
         // 1. print them in one column 1 to 5 then print a space that starts from 5 to 1 decreasing then print the numbers
-        int i,j,n=5;
-        for ( i = 1; i <= n; i++) {
+        int i, j, n = 5;
+        for (i = 1; i <= n; i++) {
             System.out.println();
             for (j = n - i; j >= 1; j--) {
                 System.out.print(" ");
@@ -31,10 +32,10 @@ public class HWork {
 
     public void printDecreasingAndIncreasingNumbersTriangle() {
         // printing 2 number patterns - in a triangle & inverted triangle side by side
-        int i, j, n=5;
+        int i, j, n = 5;
         for (i = 1; i <= n; i++) {
             System.out.println();
-            for ( j = 5 - i; j > 0; j--) {
+            for (j = 5 - i; j > 0; j--) {
                 System.out.print(" ");
             }
             for (j = 1; j <= i; j++) {
@@ -48,8 +49,8 @@ public class HWork {
 
     public void printNumbersInvertedTriangle() {
         // print numbers in inverted triangle
-        int i, j , n = 5;
-        for ( i = n; i >= 1; i--) {
+        int i, j, n = 5;
+        for (i = n; i >= 1; i--) {
             System.out.println();
             for (j = n; j >= i; j--) {
                 System.out.print(" ");
@@ -62,7 +63,7 @@ public class HWork {
         }
     }
 
-    public static void febnoacci(){
+    public static void febnoacci() {
         int n = 10, firstTerm = 0, secondTerm = 1;
         System.out.println("Fibonacci Series till " + n + " terms:");
 
@@ -78,73 +79,79 @@ public class HWork {
     }
 
 
-    public void maxMin(){
-        int[] arr = {2,6,1,45,10,12,23,54};
+    public void maxMin() {
+        int[] arr = {2, 6, 1, 45, 10, 12, 23, 54};
         int max = arr[2];
         int min = arr[2];
-        int i , j;
-        for( i = 0; i< arr.length;i++){
-            if (arr[i] > max){
-                max= arr[i];
+        int i, j;
+        for (i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
             }
-            if(arr[i]<min){
+            if (arr[i] < min) {
                 min = arr[i];
             }
         }
-        System.out.println("The maximum number is "+max+" and "+"The minimum is "+min);
+        System.out.println("The maximum number is " + max + " and " + "The minimum is " + min);
 
     }
-    public static void moreNumberPattern(){
+
+    public static void moreNumberPattern() {
         // printing in decreasing pattern triangle
-        int i, j, n=5;
-        for(i=n;i>=1;i--){
+        int i, j, n = 5;
+        for (i = n; i >= 1; i--) {
             System.out.println();
-            for(j=n;j>=i;j--){
+            for (j = n; j >= i; j--) {
                 System.out.print(" ");
             }
-            for(j=1;j<=i;j++){
+            for (j = 1; j <= i; j++) {
                 System.out.print(j);
             }
-            for(j=i-1;j>=1;j--){
+            for (j = i - 1; j >= 1; j--) {
                 System.out.print(j);
             }
-        }for(i= 1;i<=5;i++){
+        }
+        for (i = 1; i <= 5; i++) {
             System.out.println();
-            for(j=n-1;j>=i;j--){
+            for (j = n - 1; j >= i; j--) {
                 System.out.print(" ");
             }
-            for(j=1;j<=i;j++){
+            for (j = 1; j <= i; j++) {
                 System.out.print(j);
             }
-            for(j=i-1;j>=1;j--){
+            for (j = i - 1; j >= 1; j--) {
                 System.out.print(j);
             }
-    }
+        }
 
     }
-    public void sumOfNumbersInArray(){
-        int [] arr = {1,3,5,6,78,56,23,5};
-        int i,j,sum=0;
-        for(i=1;i<arr.length;i++){
-            sum+=arr[i];
+
+    public void sumOfNumbersInArray() {
+        int[] arr = {1, 3, 5, 6, 78, 56, 23, 5};
+        int i, j, sum = 0;
+        for (i = 0; i < arr.length; i++) {
+            sum += arr[i];
             System.out.print(arr[i]);
-            if(i<arr.length-1){
+            if (i < arr.length) {
                 System.out.print("+");
             }
         }
-        System.out.println("= "+sum);
-    }public static void divisor(){
+        System.out.println("= " + sum);
+    }
+
+    public static void divisor() {
         Scanner input = new Scanner(System.in);
         System.out.println("please enter a number and I will give you its divisor\n");
         int num = input.nextInt();
         ArrayList<Integer> lis = new ArrayList<Integer>();
-        for(int i =1; i<=num;i++){
-            if(num%i==0){
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) {
                 lis.add(i);
             }
         }
-        System.out.println("The divisor of "+num+" are "+lis);
+        System.out.println("The divisor of " + num + " are " + lis);
     }
+
     public static void main(String[] args) {
         sumOfNumbers();
         HWork obj1 = new HWork();
@@ -165,10 +172,39 @@ public class HWork {
         System.out.println();
         obj1.sumOfNumbersInArray();
         divisor();
+        userArrayInput();
+        twoDArray();
+
+
+        }
+
+
+    public static void userArrayInput(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your array length: ");
+        int size = input.nextInt();
+        int arr [] = new int[size];
+        System.out.println("please enter list of numbers between spaces you want to add in your array");
+        for(int i=0;i<arr.length;i++){
+            arr[i]=input.nextInt();// set up the limit of integer you accept by i (i from 0 to arr.length or size.
+        }
+        System.out.println(Arrays.toString(arr));
 
     }
+    public static void twoDArray(){
+        System.out.println("The following numbers are printed as an iterated elements of a 2-dimensional array using nested for loop");
+        int [][] array2d = {{1,4,3},{8,5,6},{7,10,19}};
+        for(int i=0;i<array2d.length;i++){
+            for(int j=0;j<array2d[i].length;j++){
+                System.out.print(array2d[i][j]+" ");
 
+           }
+        }
     }
+
+}
+
+
 
 
 
